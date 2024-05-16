45ffb9e98f331a77fc0f92512535e774bf9a0959
Edgar Sakwa <sakwaedgar57@gmail.com>
2024-05-16 12:26:37 +0300

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,14 @@
-!/usr/bin/python3
-"""
-Log parsing
-"""
-import sys
-
+#!/usr/bin/python3
 
 def print_metrics(file_size, status_codes):
     """
     Print metrics
     """
-    print("File size: {}".format(file_size))
+    print("Total file size: {}".format(file_size))
     codes_sorted = sorted(status_codes.keys())
     for code in codes_sorted:
         if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+            print(f"{code}: {status_codes[code]}")
 
 
 codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
@@ -21,24 +16,27 @@ codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
 file_size_total = 0
 count = 0
 
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            try:
-                status_code = line.split()[-2]
-                if status_code in codes_count.keys():
-                    codes_count[status_code] += 1
-                # Grab file size
-                file_size = int(line.split()[-1])
-                file_size_total += file_size
-            except Exception:
-                pass
-            # print metrics if 10 lines have been read
-            count += 1
-            if count == 10:
-                print_metrics(file_size_total, codes_count)
-                count = 0
-    except KeyboardInterrupt:
-        print_metrics(file_size_total, codes_count)
-        raise
-   print_metrics(file_size_total, codes_count)
+try:
+    while True:
+        try:
+            line = input()
+            # Assuming line starts with IP Address, Date, etc.
+            # If not, it will throw IndexError which will be handled
+            status_code = line.split()[-2]
+            if status_code in codes_count:
+                codes_count[status_code] += 1
+            # Grab file size
+            file_size = int(line.split()[-1])
+            file_size_total += file_size
+        except (IndexError, ValueError):
+            # Skip lines not in the expected format
+            pass
+        
+        # Print metrics if 10 lines have been read
+        count += 1
+        if count == 10:
+            print_metrics(file_size_total, codes_count)
+            count = 0
+except KeyboardInterrupt:
+    print_metrics(file_size_total, codes_count)
+    raise
